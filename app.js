9f07a8ee381d4a5e0d4e1d2f3b0e4ef05537ab57
Jalen Ashanti <jalenmshaw@gmail.com>
2024-07-09 10:33:48 -0400

    Completed JavaScript-Variables Exercise

--- app.js
+++ app.js
@@ -4,7 +4,35 @@ console.log(
 );
 
 // Exercise 1
+let firstName;
+let lastName;
+var age;
 
 // Exercise 2
 
+firstName = "Jalen";
+lastName = "Ashanti";
+age = 23;
+
 // Exercise 3
+
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+
+let titel = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
